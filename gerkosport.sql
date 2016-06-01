2c76093294d1c3eebf904dbc5c0563acdcb76b62
Rodrigo Cespedes Murillo <rodri.tronick@gmail.com>
2016-05-31 20:40:28 -0400

    new Database

--- gerkosport.sql
+++ gerkosport.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-03-2016 a las 20:08:06
+-- Tiempo de generación: 10-05-2016 a las 22:20:02
 -- Versión del servidor: 5.6.16
 -- Versión de PHP: 5.5.11
 
@@ -36,20 +36,20 @@ CREATE TABLE IF NOT EXISTS `alerta` (
   PRIMARY KEY (`ale_codigo`),
   KEY `ped_codigo` (`ped_codigo`),
   KEY `map_codigo` (`map_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Volcado de datos para la tabla `alerta`
 --
 
 INSERT INTO `alerta` (`ale_codigo`, `ped_codigo`, `map_codigo`, `ale_falta`, `ale_existe`, `ale_total`) VALUES
-(3, 18, 22, 248, 352, 600),
-(4, 18, 23, 4, 396, 400),
-(5, 18, 18, 118, 182, 300),
-(6, 20, 25, 380, 520, 900),
-(7, 20, 22, 1154, 46, 1200),
-(8, 20, 23, 248, 152, 400),
-(9, 20, 18, 221, 79, 300);
+(1, 3, 32, 580, 420, 1000),
+(2, 3, 28, 465, 35, 500),
+(3, 4, 32, 100, 0, 100),
+(4, 4, 28, 50, 0, 50),
+(5, 5, 44, 4500, 500, 5000),
+(6, 5, 34, 2000, 1000, 3000),
+(7, 5, 26, 1700, 300, 2000);
 
 -- --------------------------------------------------------
 
@@ -64,21 +64,32 @@ CREATE TABLE IF NOT EXISTS `almacen_materia` (
   PRIMARY KEY (`almm_codigo`),
   KEY `map_codigo` (`map_codigo`),
   KEY `map_codigo_2` (`map_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
 
 --
 -- Volcado de datos para la tabla `almacen_materia`
 --
 
 INSERT INTO `almacen_materia` (`almm_codigo`, `almm_cantidad`, `map_codigo`) VALUES
-(18, 13086, 18),
-(19, 360, 19),
-(20, 1780, 20),
-(21, 246, 21),
-(22, 1380, 22),
-(23, 702, 23),
-(24, 200, 24),
-(25, 14790, 25);
+(26, 300, 26),
+(27, 300, 27),
+(28, 15, 28),
+(29, 0, 29),
+(30, 0, 30),
+(31, 0, 31),
+(32, 900, 32),
+(33, 0, 33),
+(34, 1000, 34),
+(35, 0, 35),
+(36, 2000, 36),
+(37, 1000, 37),
+(38, 1500, 38),
+(39, 300, 39),
+(40, 200, 40),
+(41, 360, 41),
+(42, 450, 42),
+(43, 450, 43),
+(44, 2500, 44);
 
 -- --------------------------------------------------------
 
@@ -91,17 +102,23 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   `cat_nombre` varchar(100) NOT NULL,
   `cat_descripcion` varchar(300) NOT NULL,
   PRIMARY KEY (`cat_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Volcado de datos para la tabla `categoria`
 --
 
 INSERT INTO `categoria` (`cat_codigo`, `cat_nombre`, `cat_descripcion`) VALUES
-(7, 'cat1', 'des1'),
-(8, 'cat2', 'des2'),
-(9, 'cat3', 'des3'),
-(10, 'cat4', 'des4');
+(11, 'Tela Kaki', 'Material  que es utilizado para mochilas y morrales'),
+(12, 'Tela cuadriculado menudo', 'Material  que es utilizado para mochilas y morrales'),
+(13, 'Tela cuadriculado grande', 'Material utilizado para mochilas, maletines y morrales'),
+(14, 'Tela lona floreada', 'material utilizado para mochilas '),
+(15, 'Regulador', 'Accesorio para maletines y morrales'),
+(16, 'Cierre', 'Accesorio para todos los productos'),
+(17, 'Llave', 'Accesorio para todos los productos'),
+(18, 'Forro', 'accesorio para todos los productos'),
+(19, 'Correa', 'Accesorio para todos los productos'),
+(20, 'Lengueta', 'Accesorio para mochilas');
 
 -- --------------------------------------------------------
 
@@ -115,15 +132,15 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `cli_nit` int(11) NOT NULL,
   `cli_telefono` int(11) NOT NULL,
   PRIMARY KEY (`cli_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
 INSERT INTO `cliente` (`cli_codigo`, `cli_nombre`, `cli_nit`, `cli_telefono`) VALUES
-(7, 'Cliente1', 5184736, 77458745),
-(8, 'Cliente2', 2147483647, 95854525);
+(1, 'maria aguilar', 4536477, 4654345),
+(2, 'Abran Ballejos', 234567, 23456789);
 
 -- --------------------------------------------------------
 
@@ -140,24 +157,19 @@ CREATE TABLE IF NOT EXISTS `detalle_pedido` (
   KEY `pro_codigo` (`pro_codigo`),
   KEY `ped_codigo` (`ped_codigo`),
   KEY `ped_codigo_2` (`ped_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Volcado de datos para la tabla `detalle_pedido`
 --
 
 INSERT INTO `detalle_pedido` (`detp_codigo`, `ped_codigo`, `pro_codigo`, `detp_cantidad`) VALUES
-(36, 17, 15, 3),
-(37, 17, 17, 4),
-(38, 18, 15, 100),
-(39, 18, 18, 200),
-(40, 19, 15, 1),
-(41, 19, 16, 1),
-(42, 19, 17, 1),
-(43, 19, 18, 1),
-(44, 20, 15, 100),
-(45, 20, 17, 200),
-(46, 20, 18, 300);
+(1, 1, 1, 50),
+(2, 2, 1, 240),
+(3, 3, 1, 500),
+(4, 4, 1, 50),
+(5, 5, 2, 1000),
+(6, 6, 1, 60);
 
 -- --------------------------------------------------------
 
@@ -173,34 +185,29 @@ CREATE TABLE IF NOT EXISTS `detalle_salida_mp` (
   PRIMARY KEY (`dsmp_codigo`),
   KEY `smp-codigo` (`smp-codigo`),
   KEY `map_codigo` (`map_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=172 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Volcado de datos para la tabla `detalle_salida_mp`
 --
 
 INSERT INTO `detalle_salida_mp` (`dsmp_codigo`, `dsmp_cantidad`, `smp-codigo`, `map_codigo`) VALUES
-(147, 14, 41, 22),
-(148, 8, 41, 20),
-(149, 12, 41, 23),
-(150, 9, 41, 18),
-(155, 600, 43, 25),
-(156, 352, 43, 22),
-(157, 396, 43, 23),
-(158, 182, 43, 18),
-(159, 248, 44, 22),
-(160, 4, 44, 23),
-(161, 118, 44, 18),
-(162, 5, 45, 25),
-(163, 6, 45, 22),
-(164, 4, 45, 20),
-(165, 4, 45, 23),
-(166, 3, 45, 18),
-(167, 520, 46, 25),
-(168, 46, 46, 22),
-(169, 400, 46, 20),
-(170, 152, 46, 23),
-(171, 79, 46, 18);
+(1, 100, 1, 32),
+(2, 50, 1, 28),
+(3, 480, 2, 32),
+(4, 240, 2, 28),
+(5, 420, 3, 32),
+(6, 35, 3, 28),
+(7, 0, 4, 32),
+(8, 0, 4, 28),
+(9, 100, 5, 32),
+(10, 50, 5, 28),
+(11, 500, 6, 44),
+(12, 1000, 6, 34),
+(13, 300, 6, 26),
+(14, 4500, 7, 44),
+(15, 2000, 7, 34),
+(16, 1700, 7, 26);
 
 -- --------------------------------------------------------
 
@@ -216,28 +223,33 @@ CREATE TABLE IF NOT EXISTS `ingreso_materia_prima` (
   `imap_fecha_ingreso` date NOT NULL,
   PRIMARY KEY (`imap_codigo`),
   KEY `map_codigo` (`map_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Volcado de datos para la tabla `ingreso_materia_prima`
 --
 
 INSERT INTO `ingreso_materia_prima` (`imap_codigo`, `map_codigo`, `imap_cantidad`, `imap_cant_bolsa`, `imap_fecha_ingreso`) VALUES
-(30, 19, 6, 60, '2016-01-25'),
-(31, 18, 5, 40, '2016-01-26'),
-(32, 20, 40, 55, '2016-01-26'),
-(33, 21, 3, 82, '2016-01-26'),
-(34, 24, 5, 40, '2016-01-27'),
-(35, 23, 6, 70, '2016-01-27'),
-(36, 22, 10, 38, '2016-01-28'),
-(37, 25, 25, 45, '2016-01-28'),
-(38, 18, 1, 200, '2016-01-26'),
-(39, 22, 3, 100, '2016-01-26'),
-(40, 23, 2, 80, '2016-01-26'),
-(41, 25, 34, 435, '2016-03-14'),
-(42, 23, 3, 234, '2016-03-14'),
-(43, 22, 4, 345, '2016-03-14'),
-(44, 18, 6543, 2, '2016-03-14');
+(1, 26, 2, 150, '2016-04-13'),
+(2, 44, 1, 500, '2016-04-13'),
+(3, 43, 5, 90, '2016-04-13'),
+(4, 42, 5, 90, '2016-04-13'),
+(5, 41, 4, 90, '2016-04-13'),
+(6, 40, 2, 100, '2016-04-13'),
+(7, 39, 3, 100, '2016-04-13'),
+(8, 38, 3, 500, '2016-04-13'),
+(9, 37, 1, 1000, '2016-04-13'),
+(10, 27, 2, 150, '2016-04-13'),
+(11, 28, 5, 65, '2016-04-13'),
+(12, 32, 2, 500, '2016-04-13'),
+(13, 34, 1, 1000, '2016-04-13'),
+(14, 36, 2, 1000, '2016-04-13'),
+(15, 32, 2, 500, '2016-04-13'),
+(16, 28, 1, 65, '2016-04-13'),
+(17, 44, 2, 1000, '2016-04-26'),
+(18, 26, 2, 1000, '2016-05-10'),
+(19, 34, 3, 1000, '2016-05-10'),
+(20, 44, 5, 1000, '2016-05-10');
 
 -- --------------------------------------------------------
 
@@ -255,21 +267,32 @@ CREATE TABLE IF NOT EXISTS `materia_prima` (
   PRIMARY KEY (`map_codigo`),
   KEY `cat_codigo` (`cat_codigo`),
   KEY `prov_codigo` (`prov_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
 
 --
 -- Volcado de datos para la tabla `materia_prima`
 --
 
 INSERT INTO `materia_prima` (`map_codigo`, `cat_codigo`, `prov_codigo`, `map_nombre`, `map_precio`, `map_un_medida`) VALUES
-(18, 7, 11, 'mp1', 50, 'Unidades'),
-(19, 9, 10, 'mp2', 40, 'Metros'),
-(20, 7, 12, 'mp3', 80, 'Metros'),
-(21, 8, 12, 'mp4', 94, 'Unidades'),
-(22, 9, 10, 'mp5', 65, 'Metros'),
-(23, 8, 11, 'mp6', 45, 'Unidades'),
-(24, 9, 11, 'mp7', 75, 'Unidades'),
-(25, 10, 11, 'mp8', 50, 'Metros');
+(26, 11, 14, 'Kaki negro', 1500, 'Metros'),
+(27, 11, 14, 'Kaki plomo', 1500, 'Metros'),
+(28, 12, 13, 'Cuadriculado M negro', 780, 'Metros'),
+(29, 13, 13, 'Cuadriculado G negro', 840, 'Metros'),
+(30, 14, 16, 'floreado fuxia', 840, 'Metros'),
+(31, 14, 16, 'floreado lila', 840, 'Metros'),
+(32, 15, 15, 'regulador n4', 70, 'Unidades'),
+(33, 16, 14, 'cierre n5', 75, 'Metros'),
+(34, 16, 14, 'cierre n8', 55, 'Metros'),
+(35, 16, 14, 'cierre n10', 85, 'Metros'),
+(36, 17, 14, 'llave n5', 150, 'Unidades'),
+(37, 17, 14, 'llave n8', 185, 'Unidades'),
+(38, 17, 14, 'llave n10', 190, 'Unidades'),
+(39, 18, 13, 'forro antonio', 450, 'Metros'),
+(40, 18, 15, 'forro meishua', 450, 'Metros'),
+(41, 19, 15, 'corre n2 negro', 45, 'Metros'),
+(42, 11, 15, 'corre n4 negro', 65, 'Metros'),
+(43, 19, 15, 'corre n2 plomo', 55, 'Metros'),
+(44, 20, 15, 'lengueta n2', 70, 'Unidades');
 
 -- --------------------------------------------------------
 
@@ -287,17 +310,19 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   `ped_fecha_entrega` varchar(20) NOT NULL,
   PRIMARY KEY (`ped_codigo`),
   KEY `cli_codigo` (`cli_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Volcado de datos para la tabla `pedido`
 --
 
 INSERT INTO `pedido` (`ped_codigo`, `cli_codigo`, `ped_prec_total`, `ped_cant_total`, `ped_fecha`, `ped_estado`, `ped_fecha_entrega`) VALUES
-(17, 7, 1770, 7, '2016-01-26', 'PRODUCCION', '2016-02-26'),
-(18, 8, 71000, 300, '2016-01-26', 'PRODUCCION', '2016-01-29'),
-(19, 7, 915, 4, '2016-01-26', 'PRODUCCION', '2016-01-30'),
-(20, 7, 145000, 600, '2016-01-26', 'FALTA MATERIAL', '2016-02-25');
+(1, 1, 2500, 50, '2016-04-13', 'PRODUCCION', '2016-04-20'),
+(2, 1, 12000, 240, '2016-04-13', 'PRODUCCION', '2016-05-11'),
+(3, 1, 25000, 500, '2016-04-13', 'FALTA MATERIAL', '2016-04-27'),
+(4, 1, 2500, 50, '2016-04-13', 'PRODUCCION', '2016-04-21'),
+(5, 1, 60000, 1000, '2016-04-13', 'PRODUCCION', '2016-04-27'),
+(6, 2, 3000, 60, '2016-04-20', 'PENDIENTE', '2016-05-04');
 
 -- --------------------------------------------------------
 
@@ -311,17 +336,15 @@ CREATE TABLE IF NOT EXISTS `producto` (
   `pro_precio` int(11) NOT NULL,
   `pro_descripcion` varchar(200) NOT NULL,
   PRIMARY KEY (`pro_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`pro_codigo`, `pro_nombre`, `pro_precio`, `pro_descripcion`) VALUES
-(15, 'PRODUCTO A', 250, 'DESCRIPTION'),
-(16, 'PRODUCTO B', 180, 'description B'),
-(17, 'PRODUCTO C', 255, 'DEscription C'),
-(18, 'PRODUCTO D', 230, 'descripcion D');
+(1, 'morral nike', 50, 'morral nike juvenil'),
+(2, 'mochila nike', 60, 'mochila juvenil nike');
 
 -- --------------------------------------------------------
 
@@ -337,22 +360,18 @@ CREATE TABLE IF NOT EXISTS `producto_materia` (
   PRIMARY KEY (`prm_codigo`),
   KEY `pro-codigo` (`pro_codigo`),
   KEY `map_codigo` (`map_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Volcado de datos para la tabla `producto_materia`
 --
 
 INSERT INTO `producto_materia` (`prm_codigo`, `pro_codigo`, `map_codigo`, `prm_cantidad`) VALUES
-(24, 15, 18, 3),
-(25, 15, 23, 4),
-(26, 15, 22, 2),
-(27, 16, 25, 2),
-(28, 16, 20, 2),
-(29, 17, 20, 2),
-(30, 17, 22, 2),
-(31, 18, 22, 2),
-(32, 18, 25, 3);
+(1, 1, 28, 1),
+(2, 1, 32, 2),
+(3, 2, 26, 2),
+(4, 2, 34, 3),
+(5, 2, 44, 5);
 
 -- --------------------------------------------------------
 
@@ -366,16 +385,17 @@ CREATE TABLE IF NOT EXISTS `proveedor` (
   `prov_pais` varchar(50) NOT NULL,
   `prov_descripcion` varchar(100) NOT NULL,
   PRIMARY KEY (`prov_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Volcado de datos para la tabla `proveedor`
 --
 
 INSERT INTO `proveedor` (`prov_codigo`, `prov_nombre`, `prov_pais`, `prov_descripcion`) VALUES
-(10, 'proveedor1', 'pais1', 'descripcion1'),
-(11, 'proveedor2', 'pais2', 'descripcion2'),
-(12, 'proveedor3', 'pais3', 'descripcion3');
+(13, 'Elva', 'China', 'Material de baja calidad, menor precio'),
+(14, 'Ninatex', 'Peru', 'Material de buena calidad, mayor costo '),
+(15, 'Rex', 'Perú, china ', 'Material de ambas calidades, diferentes precios '),
+(16, 'Faninatex', 'Perú, china ', 'Material de ambas calidades, diferentes precios ');
 
 -- --------------------------------------------------------
 
@@ -389,19 +409,20 @@ CREATE TABLE IF NOT EXISTS `salida_materia_prima` (
   `ped_codigo` int(11) NOT NULL,
   PRIMARY KEY (`smp_codigo`),
   KEY `detp_codigo` (`ped_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Volcado de datos para la tabla `salida_materia_prima`
 --
 
 INSERT INTO `salida_materia_prima` (`smp_codigo`, `smp_fecha_salida`, `ped_codigo`) VALUES
-(41, '2016-01-26', 17),
-(42, '2016-01-26', 17),
-(43, '2016-01-26', 18),
-(44, '2016-01-26', 18),
-(45, '2016-01-26', 19),
-(46, '2016-01-26', 20);
+(1, '2016-04-13', 1),
+(2, '2016-04-13', 2),
+(3, '2016-04-13', 3),
+(4, '2016-04-13', 4),
+(5, '2016-04-13', 4),
+(6, '2016-04-13', 5),
+(7, '2016-05-10', 5);
 
 -- --------------------------------------------------------
 
@@ -419,7 +440,7 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `usu_password` varchar(20) NOT NULL,
   `usu_cargo` varchar(30) NOT NULL,
   PRIMARY KEY (`usu_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Volcado de datos para la tabla `usuario`
